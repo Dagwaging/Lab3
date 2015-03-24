981256c89d858b863620c65ca98e79ea11743fcc
Dagwaging <dagwaging@gmail.com>
2015-03-24 14:08:17 -0400

    Add FactorFactory class to pass empty result test case

---
+++ src/FactorFactory.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class FactorFactory {
+
+	public static List<Integer> getFactors(int i) {
+		return new ArrayList<Integer>();
+	}
+
+}
