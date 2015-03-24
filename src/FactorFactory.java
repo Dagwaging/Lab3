720980b0dc7bb783f87880497d1fbeb8e95196cc
Dagwaging <dagwaging@gmail.com>
2015-03-24 14:21:07 -0400

    Modify FactorFactory.getFactors to pass prime factors of 3 test case

--- src/FactorFactory.java
+++ src/FactorFactory.java
@@ -7,8 +7,13 @@ public class FactorFactory {
 	public static List<Integer> getFactors(int i) {
 		List<Integer> factors = new ArrayList<Integer>();
 
-		if(i == 2) {
+		switch(i) {
+		case 2:
 			factors.add(2);
+			break;
+		case 3:
+			factors.add(3);
+			break;
 		}
 
 		return factors;
