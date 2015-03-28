ee9afddb4f7b2cb646026b099d5de284a0a20ace
Dagwaging <dagwaging@gmail.com>
2015-03-27 21:42:01 -0400

    Add test for prime factors of 8

--- src/FactorFactoryTest.java
+++ src/FactorFactoryTest.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 import org.junit.Test;
 
-
 public class FactorFactoryTest {
 
 	@Test
@@ -15,32 +14,39 @@ public class FactorFactoryTest {
 		List<Integer> actual = FactorFactory.getFactors(1);
 		assertEquals(expected, actual);
 	}
-	
+
 	@Test
 	public void testThatTwoHasOnlyItselfAsPrimeFactor() {
 		List<Integer> expected = Arrays.asList(2);
 		List<Integer> actual = FactorFactory.getFactors(2);
 		assertEquals(expected, actual);
 	}
-	
+
 	@Test
 	public void testThatThreeHasOnlyItselfAsPrimeFactor() {
 		List<Integer> expected = Arrays.asList(3);
 		List<Integer> actual = FactorFactory.getFactors(3);
 		assertEquals(expected, actual);
 	}
-	
+
 	@Test
 	public void testThatFourHasTwoAndTwoAsPrimeFactors() {
 		List<Integer> expected = Arrays.asList(2, 2);
 		List<Integer> actual = FactorFactory.getFactors(4);
 		assertEquals(expected, actual);
 	}
-	
+
 	@Test
 	public void testThatSixHasTwoAndThreeAsPrimeFactors() {
 		List<Integer> expected = Arrays.asList(2, 3);
 		List<Integer> actual = FactorFactory.getFactors(6);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testThatEightHasTwoTwoAndTwoAsPrimeFactors() {
+		List<Integer> expected = Arrays.asList(2, 2, 2);
+		List<Integer> actual = FactorFactory.getFactors(8);
 		assertEquals(expected, actual);
 	}
 }
