b2df728feb3f852aa5788aad6f65c450cecfe429
Dagwaging <dagwaging@gmail.com>
2015-03-27 21:36:55 -0400

    Modify FactorFactory.getFactors to work more generally for prime factors of 4 test case

--- src/FactorFactory.java
+++ src/FactorFactory.java
@@ -1,19 +1,18 @@
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class FactorFactory {
 
 	public static List<Integer> getFactors(int i) {
 		List<Integer> factors = new ArrayList<Integer>();
-
-		switch(i) {
-		case 2:
+		
+		if (i % 2 == 0) {
 			factors.add(2);
-			break;
-		case 3:
-			factors.add(3);
-			break;
+			i /= 2;
+		}
+
+		if (i > 1) {
+			factors.add(i);
 		}
 
 		return factors;
