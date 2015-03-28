c055d91f0bd5391a8e2ec363297a50851a82519b
Dagwaging <dagwaging@gmail.com>
2015-03-27 22:21:53 -0400

    Add test case for primes below three, refactor tests slightly

--- src/FactorFactoryGenerateTest.java
+++ src/FactorFactoryGenerateTest.java
@@ -1,6 +1,8 @@
 import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Test;
 
@@ -9,9 +11,15 @@ public class FactorFactoryGenerateTest {
 
 	@Test
 	public void testPrimesBelowOne() {
-		ArrayList<Integer> expected = new ArrayList<Integer>();
-		ArrayList<Integer> actual = FactorFactory.generate(1);
+		List<Integer> expected = new ArrayList<Integer>();
+		List<Integer> actual = FactorFactory.generate(1);
 		assertEquals(expected, actual);
 	}
 
+	@Test
+	public void testPrimesBelowThree() {
+		List<Integer> expected = Arrays.asList(2);
+		List<Integer> actual = FactorFactory.generate(3);
+		assertEquals(expected, actual);	
+	}
 }
