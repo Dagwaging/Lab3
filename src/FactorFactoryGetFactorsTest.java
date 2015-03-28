30255054c86fd05e53d7c09e6af2156a731cbd14
Dagwaging <dagwaging@gmail.com>
2015-03-27 22:02:22 -0400

    Rename original test case to be more specific, add empty test case for FactorFactory.generate method

--- src/FactorFactoryGetFactorsTest.java
+++ src/FactorFactoryGetFactorsTest.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 import org.junit.Test;
 
-public class FactorFactoryTest {
+public class FactorFactoryGetFactorsTest {
 
 	@Test
 	public void testThatOneHasNoPrimeFactors() {
