9fdc18f61dba82fda37d0f9d575e4b5d7ad5ee00
Dagwaging <dagwaging@gmail.com>
2015-03-27 22:38:59 -0400

    Refactor FactorFactoryGenerateTest to use parameterized tests

--- src/FactorFactoryGenerateTest.java
+++ src/FactorFactoryGenerateTest.java
@@ -2,24 +2,36 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 
+@RunWith(Parameterized.class)
 public class FactorFactoryGenerateTest {
+	private int number;
+	private List<Integer> expected;
 
-	@Test
-	public void testPrimesBelowOne() {
-		List<Integer> expected = new ArrayList<Integer>();
-		List<Integer> actual = FactorFactory.generate(1);
-		assertEquals(expected, actual);
+	public FactorFactoryGenerateTest(int number, List<Integer> expected) {
+		this.number = number;
+		this.expected = expected;
 	}
-
+	
+	@Parameters
+	public static Collection<Object[]> primes() {
+		return Arrays.asList(new Object[][] {
+				{ 1, new ArrayList<Integer>() },
+				{ 3, Arrays.asList(2) }
+		});
+	}
+	
 	@Test
-	public void testPrimesBelowThree() {
-		List<Integer> expected = Arrays.asList(2);
-		List<Integer> actual = FactorFactory.generate(3);
-		assertEquals(expected, actual);	
+	public void testPrimes() {
+		List<Integer> actual = FactorFactory.generate(number);
+		assertEquals(expected, actual);
 	}
 }
